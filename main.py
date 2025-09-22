46de8c5708d8f9c66844e2bb35ad12ea0c12975e
Lucas Miranda <lucas-m@outlook.com>
2025-09-21 21:55:17 -0300

    feat: add generate recipe endpoint

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
+from typing import List
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get('/')
-def root():
-  return {'hello': 'world'}
+class RecipeRequest(BaseModel):
+    ingredients: List[str]
+    style: str
+
+@app.post('/recipes/generate')
+def generate_recipe(recipe_request: RecipeRequest):
+    print(recipe_request)
+    return {
+        "id": "uuid",
+        "title": "Frango com Batata ao Forno",
+        "ingredients": [
+            {"name": "batata", "qty": "200g"},
+            {"name": "frango", "qty": "300g"}
+        ],
+        "instructions": "1. Pré-aqueça o forno...\n2. ...",
+        "cooking_time": 45,
+        "difficulty": "fácil",
+        "image_url": "https://..."
+    }
